6e2d8b39f6beace2395010de7758c599e924a3e7
Omer Dolev <omerdolev90@gmail.com>
2019-08-28 02:35:07 +0300

    add db trigger

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,17 @@ admin.initializeApp();
 
 const functions = require('firebase-functions');
 
+const inc1 = admin.firestore.FieldValue.increment(1);
+
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.students = functions.https.onRequest(require('./students.app'));
+exports.countEntrances = functions
+    .firestore
+    .document('students/{studentId}/entrances/{entranceId}')
+    .onCreate(async (snapshot, context) => {
+        const studentRef = admin.firestore().doc(`students/${context.params.studentId}`);
+
+        await studentRef.update({ entranceCount: inc1 });
+    });
